b4c59924ec13df86b27ab60dc80d104cbe6830f6
Stellakoomson <stellakoomson006@gmail.com>
2025-01-26 22:41:18 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// function changecolor(){
+// let sit1 =document.getElementById("an");
+// sit.innerHTML = "ANNOUNCEMENT😁";
+// sit2.style.backgroundColor = "grey";
+// sit2.style.color = 'green';
+
+// }
+// let sit = document.getElementByld("an");
+// sit.addEventListener('open',changecolor);
+
